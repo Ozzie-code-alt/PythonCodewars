9d4c4a59bfc56aaf83aa3a22855d93e2afd623c1
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-08-14 21:10:53 +0800

    Codewars Submission for Python

---
+++ codewars.py
@@ -0,0 +1,23 @@
+# Reverse a String
+
+# def solution(string):
+#      print(string[::-1])  this is slicing method
+        
+
+# solution('Hello world')  
+
+#for loop method
+
+# def reverse(string):
+#     original_String = string
+#     reverse_string = ""
+
+#     for char in original_String:
+        
+#         reverse_string = char + reverse_string
+#         print(reverse_string) check this to see how it works 
+
+#     print(reverse_string)
+
+
+# reverse("Justin")
